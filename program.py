fa3a12bc71d070a0513e1580c66d9ecbf3a238b5
icosane <horowheattail@gmail.com>
2021-02-09 20:41:53 +0400

    Remove duplicated directory

--- program.py
+++
@@ -1,8 +0,0 @@
-import numpy as np, matplotlib.pyplot as plt, seaborn as sns, mpld3
-%matplotlib inline
-sns.set_context("poster")
-sns.set_style('whitegrid')
-
-plt.plot([3,1,4,1,5,9,6,2], 'o-')
-plt.title('Hello, World')
-mpld3.save_json(plt.gcf(), 'test.json')
