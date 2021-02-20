54ea75a4d3c7ab32b2543850c218253302c9e032
icosane <horowheattail@gmail.com>
2021-02-20 16:10:33 +0400

    ~

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('graph', views.home, name='home'),
     path('another', views.another, name='another'),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
