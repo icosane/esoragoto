ec80c941093d78eb4525bcead057451f190b03ef
icosane <horowheattail@gmail.com>
2021-03-03 12:25:18 +0400

    fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#%wu1i3d_tn_p+ej0o19ejb_o!_!t22n#paa7vlrp2dokms%0@'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
