53aa81d6c2ba0eb46dad1a6acafaa78b79808d89
icosane <horowheattail@gmail.com>
2021-04-01 15:40:27 +0400

    ph

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','unsplash.com']
 
 
 # Application definition
