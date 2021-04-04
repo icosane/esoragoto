00d14713f149ad953d277a65b8746bbcfee9407c
icosane <horowheattail@gmail.com>
2021-04-04 12:48:29 +0400

    test buttons

--- esoragoto/urls.py
+++ esoragoto/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('another2', views.another, name='another2'),
     path('offline', views.offline, name='offline'),
     path('graph_n1', views.graph_n1, name='graph_n1'),
+    path('graph_n11', views.graph_n11, name='graph_n11'),
     path('graph_n2', views.graph_n2, name='graph_n2'),
     path('', include('pwa.urls')),
 ]
