ebc8df743efa7cfa0cddaff2b2b32e306af831f8
icosane <horowheattail@gmail.com>
2021-04-05 17:53:51 +0400

    meow

--- esoragoto/views.py
+++ esoragoto/views.py
@@ -9,188 +9,7 @@ from esoragoto.forms import TextForm
 def offline(request):
     return render(request, 'offline.html')
 
-    
-def home(request):
-    N = 10  # число осциляторов
-    K = 3  # K/N = параметр амплитуды связи
-    interval_t = 100  # пространство i
-    l = random.uniform(0, 2 * pi, N)
-    theta0 = []  # начальные фазы
-    for i in l:
-        theta0.append('%.1f' % i)  # количество точек после запятой
 
-    # omega = int2.99
-    omega = random.uniform(1, 3, N)  # частота
-
-    # omega = [1.1, 1.12, 1.13, 1.16, 1.18]
-
-    def kuramoto(theta, t, omega, K, N):
-        A, B = sin(theta), cos(theta)
-        return omega + (K / N) * (B * sum(A) - A * sum(B))
-
-    t = linspace(0, 2, interval_t)  # время
-
-    theta = odeint(kuramoto, theta0, t, args=(omega, K, N))  # решение системы диф. уравнений 1 порядка
-
-    # вычисление r(t)
-    S1 = [sum(cos(theta[i])) for i in range(interval_t)]
-    d1 = array([i ** 2 for i in S1])
-
-    S2 = [sum(sin(theta[i])) for i in range(interval_t)]
-    d2 = array([i ** 2 for i in S2])
-
-    r = (1.0 / N) * sqrt(d1 + d2)
-
-    # построение графиков
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    for i in range(N):
-        ax1.plot(t, sin(omega[i] * t + theta[:, i]))
-    ax1.set(xlabel='t', ylabel='sin(ω(t)+θ)')
-
-    ax2.plot(t, r)
-    ax2.set(xlabel='t', ylabel='r (t)')
-
-    ax1.grid()
-    ax2.grid()
-    fig.tight_layout()
-    html_fig = mpld3.fig_to_html(fig, template_type='general')
-
-    plt.close(fig)
-
-    return render(request, 'graph.html', {'active_page': 'graph.html', 'div_figure': html_fig})
-
-def another(request):
-    N_ = int(request.POST['N'])
-    K = int(request.POST['K'])
-    interval_t = 100  # пространство i
-    l = random.uniform(0, 2 * pi, N_)
-    theta0 = []  # начальные фазы
-    for i in l:
-        theta0.append('%.1f' % i)  # количество точек после запятой
-
-    # omega = int2.99
-    omega = random.uniform(1, 3, N_)  # частота
-
-    # omega = [1.1, 1.12, 1.13, 1.16, 1.18]
-
-    def kuramoto(theta, t, omega, K, N_):
-        A, B = sin(theta), cos(theta)
-        return omega + (K / N_) * (B * sum(A) - A * sum(B))
-
-    t = linspace(0, 2, interval_t)  # время
-
-    theta = odeint(kuramoto, theta0, t, args=(omega, K, N_))  # решение системы диф. уравнений 1 порядка
-
-    # вычисление r(t)
-    S1 = [sum(cos(theta[i])) for i in range(interval_t)]
-    d1 = array([i ** 2 for i in S1])
-
-    S2 = [sum(sin(theta[i])) for i in range(interval_t)]
-    d2 = array([i ** 2 for i in S2])
-
-    r = (1.0 / N_) * sqrt(d1 + d2)
-
-    # построение графиков
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    for i in range(N_):
-        ax1.plot(t, sin(omega[i] * t + theta[:, i]))
-    ax1.set(xlabel='t', ylabel='sin(ω(t)+θ)')
-
-    ax2.plot(t, r)
-    ax2.set(xlabel='t', ylabel='r (t)')
-
-    ax1.grid()
-    ax2.grid()
-    fig.tight_layout()
-    html_fig = mpld3.fig_to_html(fig, template_type='general')
-
-    plt.close(fig)
-
-    return render(request, 'another.html', {'div_figure': html_fig})
-
-def home2(request):
-    N = 10
-    m = 50
-    K = linspace(0,8,m)
-    interval_t = 100  #пространство i
-
-    theta0 = random.uniform(0,2*pi,N)
-    omega = random.uniform(1,5,N)
-    def kuramoto(theta, t, omega, K, N_):
-        A, B = sin(theta), cos(theta)
-        return omega + (K / N_) * (B * sum(A) - A * sum(B))
-
-    t = linspace(0, 2, interval_t)  # время
-
-    L = []
-
-    for j in K:
-        theta = odeint(kuramoto,theta0,t,args=(omega,j,N))
-
-        S1 = [sum(cos(theta[i])) for i in range(interval_t)]
-        d1 = array([i ** 2 for i in S1])
-
-        S2 = [sum(sin(theta[i])) for i in range(interval_t)]
-        d2 = array([i ** 2 for i in S2])
-
-        r = (1.0 / N) * sqrt(d1 + d2)
-
-        x = r[len(r)-1]
-        L.append(x)
-
-    #построение графика
-    fig, ax = plt.subplots()
-    #for i in range(m-1):
-    ax.plot(K, L)
-    ax.set(xlabel='K', ylabel='r ∞')
-
-    ax.grid()
-    html_fig = mpld3.fig_to_html(fig, template_type='general')
-
-    plt.close(fig)
-    return render(request, 'graph2.html', {'div_figure': html_fig})
-
-def another2(request):
-    N = int(request.POST['N'])
-    m = 50
-    K = linspace(0,8,m)
-    interval_t = 100  #пространство i
-
-    theta0 = random.uniform(0,2*pi,N)
-    omega = random.uniform(1,5,N)
-    def kuramoto(theta, t, omega, K, N_):
-        A, B = sin(theta), cos(theta)
-        return omega + (K / N_) * (B * sum(A) - A * sum(B))
-
-    t = linspace(0, 2, interval_t)  # время
-
-    L = []
-
-    for j in K:
-        theta = odeint(kuramoto,theta0,t,args=(omega,j,N))
-
-        S1 = [sum(cos(theta[i])) for i in range(interval_t)]
-        d1 = array([i ** 2 for i in S1])
-
-        S2 = [sum(sin(theta[i])) for i in range(interval_t)]
-        d2 = array([i ** 2 for i in S2])
-
-        r = (1.0 / N) * sqrt(d1 + d2)
-
-        x = r[len(r)-1]
-        L.append(x)
-
-    #построение графика
-    fig, ax = plt.subplots()
-    #for i in range(m-1):
-    ax.plot(K, L)
-    ax.set(xlabel='K', ylabel='r ∞')
-
-    ax.grid()
-    html_fig = mpld3.fig_to_html(fig, template_type='general')
-
-    plt.close(fig)
-    return render(request, 'another2.html', {'div_figure': html_fig})
 
 #<------------------------------------------------------------------------->
 
@@ -275,54 +94,7 @@ def index(request):
     plt.close(fig2)
     return render(request, 'index.html', {'div_figure1': html_fig1, 'div_figure2': html_fig2})
 
-def graph_n1(request):
-    N_ = int(request.POST['N'])
-    K = int(request.POST['K'])
-    interval_t = 100  # пространство i
-    l = random.uniform(0, 2 * pi, N_)
-    theta0 = []  # начальные фазы
-    for i in l:
-        theta0.append('%.1f' % i)  # количество точек после запятой
 
-    # omega = int2.99
-    omega = random.uniform(1, 3, N_)  # частота
-
-    # omega = [1.1, 1.12, 1.13, 1.16, 1.18]
-    @numba.njit
-    def kuramoto(theta, t, omega, K, N_):
-        A, B = sin(theta), cos(theta)
-        return omega + (K / N_) * (B * sum(A) - A * sum(B))
-
-    t = linspace(0, 2, interval_t)  # время
-
-    theta = odeint(kuramoto, theta0, t, args=(omega, K, N_))  # решение системы диф. уравнений 1 порядка
-
-    # вычисление r(t)
-    S1 = [sum(cos(theta[i])) for i in range(interval_t)]
-    d1 = array([i ** 2 for i in S1])
-
-    S2 = [sum(sin(theta[i])) for i in range(interval_t)]
-    d2 = array([i ** 2 for i in S2])
-
-    r = (1.0 / N_) * sqrt(d1 + d2)
-
-    # построение графиков
-    fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(15,6))
-    for i in range(N_):
-        ax1.plot(t, sin(omega[i] * t + theta[:, i]))
-    ax1.set(xlabel='t', ylabel='sin(ω(t)+θ)')
-
-    ax2.plot(t, r)
-    ax2.set(xlabel='t', ylabel='r (t)')
-
-    ax1.grid()
-    ax2.grid()
-    fig.tight_layout()
-    html_fig = mpld3.fig_to_html(fig, template_type='general')
-
-    plt.close(fig)
-
-    return render(request, 'graph_n1.html', {'div_figure': html_fig})
 
 def ru(request):
     N1 = 50  # число осциляторов
@@ -455,7 +227,55 @@ def graph_n1(request):
 
     return render(request, 'graph_n1.html', {'div_figure': html_fig})
 
+def graph_n1ru(request):
+    N_ = int(request.POST['N'])
+    K = int(request.POST['K'])
+    interval_t = 100  # пространство i
+    t_ = float(request.POST['t'])
+    l = random.uniform(0, 2 * pi, N_)
+    theta0 = []  # начальные фазы
+    for i in l:
+        theta0.append('%.1f' % i)  # количество точек после запятой
 
+    # omega = int2.99
+    omega = random.uniform(1, 3, N_)  # частота
+
+    # omega = [1.1, 1.12, 1.13, 1.16, 1.18]
+    @numba.njit
+    def kuramoto(theta, t, omega, K, N_):
+        A, B = sin(theta), cos(theta)
+        return omega + (K / N_) * (B * sum(A) - A * sum(B))
+
+    t = linspace(0, t_, interval_t)  # время
+
+    theta = odeint(kuramoto, theta0, t, args=(omega, K, N_))  # решение системы диф. уравнений 1 порядка
+
+    # вычисление r(t)
+    S1 = [sum(cos(theta[i])) for i in range(interval_t)]
+    d1 = array([i ** 2 for i in S1])
+
+    S2 = [sum(sin(theta[i])) for i in range(interval_t)]
+    d2 = array([i ** 2 for i in S2])
+
+    r = (1.0 / N_) * sqrt(d1 + d2)
+
+    # построение графиков
+    fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(15,6))
+    for i in range(N_):
+        ax1.plot(t, sin(omega[i] * t + theta[:, i]))
+    ax1.set(xlabel='t', ylabel='sin(ω(t)+θ)')
+
+    ax2.plot(t, r)
+    ax2.set(xlabel='t', ylabel='r (t)')
+
+    ax1.grid()
+    ax2.grid()
+    fig.tight_layout()
+    html_fig = mpld3.fig_to_html(fig, template_type='general')
+
+    plt.close(fig)
+
+    return render(request, 'graph_n1ru.html', {'div_figure': html_fig})
 
 
 def graph_n2(request):
@@ -502,3 +322,46 @@ def graph_n2(request):
     plt.close(fig)
     return render(request, 'graph_n2.html', {'div_figure': html_fig})
 
+def graph_n2ru(request):
+    N = int(request.POST['N'])
+    m = int(request.POST['m'])
+    t__ = int(request.POST['t'])
+    K = linspace(0,t__,m)
+    interval_t = 100  #пространство i
+
+    theta0 = random.uniform(0,2*pi,N)
+    omega = random.uniform(1,5,N)
+    @numba.njit
+    def kuramoto(theta, t, omega, K, N_):
+        A, B = sin(theta), cos(theta)
+        return omega + (K / N_) * (B * sum(A) - A * sum(B))
+
+    t = linspace(0, 2, interval_t)  # время
+
+    L = []
+
+    for j in K:
+        theta = odeint(kuramoto,theta0,t,args=(omega,j,N))
+
+        S1 = [sum(cos(theta[i])) for i in range(interval_t)]
+        d1 = array([i ** 2 for i in S1])
+
+        S2 = [sum(sin(theta[i])) for i in range(interval_t)]
+        d2 = array([i ** 2 for i in S2])
+
+        r = (1.0 / N) * sqrt(d1 + d2)
+
+        x = r[len(r)-1]
+        L.append(x)
+
+    #построение графика
+    fig, ax = plt.subplots()
+    #for i in range(m-1):
+    ax.plot(K, L)
+    ax.set(xlabel='K', ylabel='r ∞')
+
+    ax.grid()
+    html_fig = mpld3.fig_to_html(fig, template_type='general')
+
+    plt.close(fig)
+    return render(request, 'graph_n2ru.html', {'div_figure': html_fig})
--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('offline', views.offline, name='offline'),
     path('graph_n1', views.graph_n1, name='graph_n1'),
     path('graph_n2', views.graph_n2, name='graph_n2'),
+    path('graph_n1ru', views.graph_n1ru, name='graph_n1ru'),
+    path('graph_n2ru', views.graph_n2ru, name='graph_n2ru'),
     path('', include('pwa.urls')),
     #path('admin/', admin.site.urls),
 ]
