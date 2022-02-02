f488d0fe4c0e34f3af77135dfdb15f1bcacc546f
icosane <miiyukiiiiii@gmail.com>
2022-02-02 15:25:43 +0400

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['esoragoto.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,4 +135,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+WHITENOISE_AUTOREFRESH = True
 
